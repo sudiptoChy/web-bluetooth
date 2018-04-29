e558bc8159837db1c597cbcdcd23a637d1d00d36
SELISE\sudipto.chowdhury <sudipto.chowdhury@selise.ch>
2018-04-29 13:28:46 +0600

    initial commit

---
+++ app.js
@@ -0,0 +1,29 @@
+/**
+ * Author: Sudipto Chowdhury
+ * Email: dip.chy93@gmail.com
+ * Software Engineer, SELISE
+ */
+
+function fireBluetooth () {
+    console.log('Requesting Bluetooth Device...');
+    navigator.bluetooth.requestDevice(
+        {filters: [{services: ['battery_service']}]})
+        .then(device => {
+        console.log('Connecting to GATT Server...');
+    return device.gatt.connect();
+    }).then(server => {
+        console.log('Getting Battery Service...');
+    return server.getPrimaryService('battery_service');
+    }).then(service => {
+        console.log('Getting Battery Level Characteristic...');
+        return service.getCharacteristic('battery_level');
+    }).then(characteristic => {
+        console.log('Reading Battery Level...');
+        return characteristic.readValue();
+    }).then(value => {
+            let batteryLevel = value.getUint8(0);
+        log('> Battery Level is ' + batteryLevel + '%');
+    }).catch(error => {
+        console.log('Argh! ' + error)
+    });
+}
